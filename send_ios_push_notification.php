f412c2b30fcb4a7602b0043dee6454f7b2874de9
Norhaina Karon <hainaalfonso@gmail.com>
2019-04-07 10:11:50 +0800

    Implemented proper description
    Implemented proper description of parts that needs to be changed by the user in order to properly inject this code into their projects. All values that should be changed are enclosed with asterisks(*) with texts that starts with the word "insert."

--- send_ios_push_notification.php
+++ send_ios_push_notification.php
@@ -1,12 +1,12 @@
 <?
-$deviceToken = '{insert device token here}';
+$deviceToken = '*insert device token here*';
 	
 $url = 'ssl://gateway.sandbox.push.apple.com:2195'; //use this for development
 //$url = 'ssl://gateway.push.apple.com:2195'; //use this for production
 
 $ctx = stream_context_create();
-stream_context_set_option ( $ctx, 'ssl', 'local_cert', '{path / name of your pem file}'.pem ); 
-stream_context_set_option ( $ctx, 'ssl', 'passphrase', '{password for your pem file}' );
+stream_context_set_option ( $ctx, 'ssl', 'local_cert', '*path / name of your pem file*'.pem ); 
+stream_context_set_option ( $ctx, 'ssl', 'passphrase', '*insert the password for your pem file*' );
 
 $fp = stream_socket_client (
 	$url, $err,
@@ -20,9 +20,9 @@ echo 'Connected to APNS';
 
 
 $body['aps'] = [
-	'alert' => '{ your message here }',
-	'sound' => '{ name of the custom notification sound }',
-	'url' => '{ link to be opened when the notification is clicked }'
+	'alert' => '*insert your message here*',
+	'sound' => '*insert the name of the custom notification sound*',
+	'url' => '*insert the link to be opened when the notification is clicked*'
 ];
 
 $payload = json_encode ( $body );
